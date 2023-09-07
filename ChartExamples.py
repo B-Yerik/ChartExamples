7e684ebc7a9b3a3be3955ffdc2fe9d3d232ccefb
B-Yerik <140368858+B-Yerik@users.noreply.github.com>
2023-09-07 19:33:26 +0600

    Create ChartExamples.py
    Sozdanie osnovy programmy

---
+++ ChartExamples.py
@@ -0,0 +1,14 @@
+#пример построения графиков
+
+import tkinter as tk
+
+#sozdanie novogo okna
+window = tk.Tk()
+window.geometry("450x450")
+window.title("Primery postroenie grafikov")
+
+#Zapusk cikla mainloop
+window.mainloop
+
+
+
