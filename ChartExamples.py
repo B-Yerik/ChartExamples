bbe60b56add7c9b5c518c0ac66c5785fcdcc3d7c
B-Yerik <140368858+B-Yerik@users.noreply.github.com>
2023-09-08 06:54:04 +0600

    dobavlena knopka zakrytiya

--- ChartExamples.py
+++ ChartExamples.py
@@ -2,10 +2,18 @@
 
 import tkinter as tk
 
+#funkicya zakrytiya
+def do_close():
+    window.destroy()
+
 #sozdanie novogo okna
 window = tk.Tk()
 window.geometry("450x450")
 window.title("Primery postroenie grafikov")
+
+#dobavlenir knopki zakrytiya
+btnClose = tk.Button(window, text='Close', font=('Helvetica',10,'bold'),command=do_close)
+btnClose.place(x=330, y=400, width=90, height=30)
 
 #Zapusk cikla mainloop
 window.mainloop()
